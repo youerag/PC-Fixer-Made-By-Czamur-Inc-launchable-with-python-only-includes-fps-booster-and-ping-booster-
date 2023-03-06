6d276761b226b0290f5d0c1680d57de344b761a5
youerag <125828078+youerag@users.noreply.github.com>
2023-03-06 16:15:24 +0000

    Add files via upload
    join discord server @ http://dsc.gg/czamur

---
+++ CzamurIncFreePing.py
@@ -0,0 +1,49 @@
+MainCraft = "CzamurIncFreePing.repl.py"
+
+print('Starting ', MainCraft)
+print('Accessing processor..')
+input("Are you ready to boot this repl?: ")
+print('Accessing .minecraft')
+
+
+print('Accessing .roblox')
+
+print('Accessing BadlionxLunarxSalwyrr.exe')
+
+print('Removing all trust-core.xyz programs.')
+
+input("Do you have a trust-core.xyz popup from time to time?: ")
+
+print('Saving all accessed files')
+print('Flushing trust-core.xyz down the toilet')
+
+input("Are you a gamer?: ")
+print('OK')
+
+input("Do you need an FPS boost (going from 30 to 1200 fps): ")
+print('OK')
+input("Do you need your ping fixed?: ")
+print('OK')
+print('Working on your answers..')
+print('Saving DLL of all accessed files')
+print('Deleting accessed files')
+print('Dragging them our of the recycling bin')
+input("CzamurInc needs permission to reinstall the DLL, if you say no, it can cause a big problem to your computer/laptop.: ")
+print('OK')
+print('Rebooting DLLs to their place')
+print('Accessing all BAT files')
+print('System32 fix..')
+print('Error Code 640')
+input("CzamurInc needs permission to reboot your BAT files, the answer no can cause big damage to your computer/laptop.: ")
+print('Fixing general FPS and PING')
+print('Checking for games...')
+print('Found 21 gaming files...')
+print('Fixing gaming FPS and PING')
+input("CzamurInc needs permission to modify your computer, your boost won't work if you don't say yes: ")
+print('Installing Lunar Client mods')
+print('Installing EzPing onto your minecraft')
+print('Vape V4 fps boost modules..')
+print('!!THIS PROGRAM IS MADE BY YOUERAG, IF YOU NEED TO CONTACT YOUERAG ON DISCORD THEN ADD HIS DISCORD @ raggy#8163')
+input("You have to type yes and then click enter to let Czamur Inc send the last message: ")
+print('Consider joining the discord server @ dsc.gg/czamur')
+print('You can close the program now.')
